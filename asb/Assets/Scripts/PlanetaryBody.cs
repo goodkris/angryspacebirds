a8bda5b88c78fa5c0e96f749299d649eeafe8093
Srikrishna <goodkris@yahoo.com>
2019-10-19 11:25:41 -0700

    velocity and rotation template

--- asb/Assets/Scripts/PlanetaryBody.cs
+++ asb/Assets/Scripts/PlanetaryBody.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlanetaryBody : MonoBehaviour
 {
+
+    public Vector3 Velocity;
+    public Vector3 InitialVelocity;
+    public float rotationSpeed = 0.5f;
+    public float Mass = 1;
+    public float Size = 1;
+    public int StepSize = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,9 @@ public class PlanetaryBody : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // update object's position based on velocity
+        gameObject.transform.position += Velocity;
+
+        gameObject.transform.Rotate(new Vector3(0, rotationSpeed, 0));
     }
 }
--- asb/Assets/Scripts/SimCore.cs
+++ asb/Assets/Scripts/SimCore.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SimCore : MonoBehaviour
 {
+
+    public List<GameObject> PlanetaryBodies; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class SimCore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i=0;i<PlanetaryBodies.Count;i++)
+        {
+            // update velocity vector on each planetary body.
+
+            var pb = PlanetaryBodies[i].GetComponent<PlanetaryBody>();
+            float updatedX = 0;
+            float updatedY = 0;
+            float updatedZ = 0;
+
+            // figure out the updated velocity vector after calculations...
+            // calculations here!
+
+            var newVelocity = new Vector3(updatedX, updatedY, updatedZ);
+            pb.Velocity = newVelocity;
+        }
     }
 }
