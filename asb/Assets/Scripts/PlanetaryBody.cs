c274ce6224e87bb5596eb2cb2f2e1625a68bb75d
Lexie <lexiechau@gmail.com>
2019-10-19 21:12:21 -0700

    added pos vectores in body

--- asb/Assets/Scripts/PlanetaryBody.cs
+++ asb/Assets/Scripts/PlanetaryBody.cs
@@ -10,14 +10,19 @@ public class PlanetaryBody : MonoBehaviour
     public float rotationSpeed = 0.5f;
     public float Mass = 1;
     public float Size = 1;
-    public int StepSize = 1;
-    public float simStepTime = 60.0F;
-    public float Massn1;
+    public float simStepTime = 3600.0F;
+    public double posx = 0.0;
+    public double posy = 0.0;
+    public double posz = 0.0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Massn1 = 1 / Mass;
+        posx = gameObject.transform.position.x;
+        posy = gameObject.transform.position.y;
+        posz = gameObject.transform.position.z;
+
     }
 
     // Update is called once per frame
