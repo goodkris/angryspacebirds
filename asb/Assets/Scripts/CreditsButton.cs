d150b3c7e7dff280e448fe66d03d2424f1e59d7d
lanly_dev <lanly-dev@users.noreply.github.com>
2019-10-20 11:00:56 -0700

    CredisButton and InstButton

---
+++ asb/Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CreditsButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text creditsText;
+    void Start()
+    {
+        creditsText = GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        creditsText.text = "Angry Space Bird??";
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        creditsText.text =  "Credits";
+    }
+}
---
+++ asb/Assets/Scripts/InstButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InstButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text insText;
+    void Start()
+    {
+        insText = GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        insText.text =  "Instruction details hotkeys?";
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        insText.text = "Instruction";
+    }
+}
